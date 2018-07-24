de6bc7d11ffba37b6e3bee4f9cc5b2e4d1ef373d
Emily Welsch <emwelsch@gmail.com>
2018-07-24 18:35:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "F", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "F", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "M", "brown", "chillax", TRUE;
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "brown", "chillax", TRUE;
-INSERT INTO bears (age, gender, color, temperament, alive) VALUES (12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "brown", "mild", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "F", "black", "aggressive", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "uptight", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "F", "black", "annoyed", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "M", "brown", "happy", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "black", "sleepy", TRUE;
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (13, "M", "brown", "doopy", TRUE;
