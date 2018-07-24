598b33111db8256b7b853159cac1284879fcbe43
Emily Welsch <emwelsch@gmail.com>
2018-07-24 18:05:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "chillax", TRUE;
